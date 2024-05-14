a429568e93c0fa3c131b3e8cbb8c565296031278
Linjston <1181988751@qq.com>
2024-05-14 22:42:34 +0800

    去除注释

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: 空白不算白 537718696@qq.com
- * @Date: 2024-05-14 22:27:46
- * @LastEditors: 空白不算白 537718696@qq.com
- * @LastEditTime: 2024-05-14 22:32:38
- * @FilePath: \multi-person-order-restaurant\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
